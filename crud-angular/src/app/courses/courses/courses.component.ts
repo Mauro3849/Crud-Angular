1a9679c5378ca1a287e1a2429a6dea3bcbbd4f05
Mauro3849 <mdornelassilva@gmail.com>
2025-01-15 14:01:53 -0300

    Inclusao de Interface e datatable

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../models/course';
+
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses:Course[]=[{_id:'1',name:'angular',category:'backend'}];
+  displayedColumns=['name','category']
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
